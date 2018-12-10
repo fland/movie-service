db3f4a6220a79ed2fba3bb0bd678fdcbe3518cd6
fland <fland@users.noreply.github.com>
2018-12-10 23:57:36 +0100

    + moved services to separate packages

--- comment-service/src/main/java/org/movie/service/comment/Main.java
+++ comment-service/src/main/java/org/movie/service/comment/Main.java
@@ -1,4 +1,4 @@
-package org.movie.service;
+package org.movie.service.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- movie-details/src/main/java/org/movie/service/movie_detail/service/MovieService.java
+++ movie-details/src/main/java/org/movie/service/movie_detail/service/MovieService.java
@@ -1,4 +1,4 @@
-package org.movie.service.service;
+package org.movie.service.movie_detail.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
