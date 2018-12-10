176aac48a3b8ef10437f4d57e6c0d63e1b6e5e6b
fland <fland@users.noreply.github.com>
2018-12-10 23:47:21 +0100

    + movie-details service skeleton

---
+++ movie-details/src/main/java/org/movie/service/service/MovieService.java
@@ -0,0 +1,22 @@
+package org.movie.service.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Maksym Bondarenko
+ * @version 1.0 10.12.18
+ */
+
+@RestController
+@Slf4j
+public class MovieService {
+
+    @RequestMapping(value = "/movie/{uuid}")
+    public String get(@PathVariable String uuid) {
+        log.info("Movie uuid: {}", uuid);
+        return "{}";
+    }
+}
